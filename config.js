b9df09eb67f1a1eba7aebb506ec480619c7f997c
Bram <bram@typify.com>
2021-04-16 15:00:16 +0200

    config file

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    configuration: {
+        'bot_token':'',
+        'bot_key':'$',
+        'db_host':'localhost',
+        'db_user':'db_user',
+        'db_database':'database',
+        'db_password':'password'
+    }
+}
